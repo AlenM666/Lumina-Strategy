dca266c0ff801d3602bed883c2dd78103e441faf
AlenM666 <llolek278@gmail.com>
2023-03-14 03:34:55 +0100

    fixed footer collers and borders

--- src/partials/Newsletter.jsx
+++ src/partials/Newsletter.jsx
@@ -34,8 +34,11 @@ function Newsletter() {
             {/* CTA form */}
             <form className="w-full lg:w-1/2">
               <div className="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:max-w-none">
-                <input type="email" className="w-full appearance-none bg-purple-700 border border-purple-100 focus:border-purple-300 rounded-full px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-purple-100" placeholder="Your email…" aria-label="Your email…" />
-                <a className="btn text-purple-600 bg-purple-100 hover:bg-white shadow rounded-full" href="#0">Subscribe</a>
+                <input type="email" className="w-full appearance-none bg-purple-600 border-2 border-gray-100 focus:border-gray-100 rounded-full px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-100" placeholder="Your email…" aria-label="Your email…" />
+                {/*need to add path for subscribe button */}
+                {/*add it so when pressed to send request to gmail and send a email to inputed mail */}
+                {/* something like OnClick function or gmail api, check*/ }
+                <a className="btn text-purple-600 bg-gray-100 hover:bg-white shadow rounded-full" href="">Subscribe</a>
               </div>
               {/* Success message */}
               {/* <p className="text-center lg:text-left lg:absolute mt-2 opacity-75 text-sm">Thanks for subscribing!</p> */}
